12fc99e2b2dbfae74d6b8686590d6ad961a2c21c
kimnh16 <31956252+kimnh16@users.noreply.github.com>
2017-10-22 09:13:02 +0900

    Update day13_hw.py

--- day13_hw.py
+++ day13_hw.py
@@ -10,7 +10,17 @@ def wall():
     for x in range(4):
         t.forward(500)
         t.left(90)
-
+        
+# 장애물 그리는 함수(추가함)
+def obstacle():
+    t.speed(0)
+    t.up()
+    t.goto(-150,0)
+    t.down()
+    for x in range(4):
+        t.forward(100)
+        t.left(90)
+    
 # 랜덤의 각도로 출발하는 함수
 def start():
     angle = random.randint(0, 360)
@@ -22,7 +32,7 @@ def start():
         t.forward(1)
 
 # 벽에 반사하는 함수(수정해야함)
-def reflect():
+def reflect1():
     ang = t.heading()
     t.speed(0)
     if 0 <= ang < 90:
@@ -45,7 +55,33 @@ def reflect():
             t.left(2 * (ang - 180))
         else:
             t.right(2 * (ang - 270))
-
+            
+       
+            
+# 벽에 반사하는 함수 (추가함)
+def reflect():
+    ang = t.heading()
+    t.speed(0)
+    if t.ycor() == 250 or t.ycor == -250:
+        t.setheading(360 - ang)
+    elif 0 <= ang < 180:
+        t.setheading(180 - ang)
+    else:
+        t.setheading(540 - ang)
+    
+# 장애물에 반사하는 함수 (추가함)
+def reflect2():
+    ang2 = t.heading()
+    t.speed(0)
+    if t.ycor() == 0 or t.ycor() == 100:
+        t.setheading(360 - ang2)
+    elif 0 <= ang2 < 180:
+        t.setheading(180 - ang2)
+    else:
+        t.setheading(540 - ang2)
+        
+     
+        
 # 최고속도로 가는 함수
 def best():
     while -250 <= t.xcor() <= 250 and -250 <= t.ycor() <= 250:
@@ -63,7 +99,7 @@ def slow():
     while -250 <= t.xcor() <= 250 and -250 <= t.ycor() <= 250:
         t.speed(1)
         t.forward(1)
-        
+       
 wall()
 
 t.up()
@@ -73,4 +109,15 @@ t.shape("turtle")
 
 start()
 reflect()
-print(ang)
+
+# 만약 거북이가 향하는 방향이 장애물을 지난다면 reflect2()를 실행(코딩짜기)
+
+for i in range(10):
+    while -250 <= t.xcor() <= 250 and -250 <= t.ycor() <= 250:
+        if t.xcor() == -150 or t.xcor() == -50 or t.ycor() == 0 or t.ycor() == 100:
+            reflect2()
+            best()        
+        else:
+            t.speed(10 - i) #10번 반복할때만 오류 안남
+            t.forward(1)
+    reflect()
